8fd3701742357c5bc55b2a19184f6779750d5115
jackietchen <hcchcxl@163.com>
2015-05-19 00:00:56 +0800

    db完善user

--- src/db/DB.java
+++ src/db/DB.java
@@ -36,7 +36,48 @@ public class DB {
 			return null;
 		}
 	}
-	
+	public boolean insertUser(String username,String pwd,String authority){
+		try{
+			pstmt=ct.prepareStatement("select username from user where username=?");
+			pstmt.setString(1, username);
+			ResultSet rs=pstmt.executeQuery();
+			while(rs.next()){  return false;}
+			pstmt=ct.prepareStatement("insert into user (username, password,authority) values(?,?,?)");
+			pstmt.setString(1, username);
+			pstmt.setString(2, pwd);
+			pstmt.setString(3, authority);
+			pstmt.executeUpdate();
+			return true;
+		}catch(Exception e){
+			e.printStackTrace();
+			return false;
+		}
+	}
+	public boolean updateUser(String username,String pwd,String authority){
+		try{
+			pstmt=ct.prepareStatement("update user set password= ?, authority = ? where username = ?");
+			pstmt.setString(1, username);
+			pstmt.setString(2, pwd);
+			pstmt.setString(3, authority);
+			pstmt.executeUpdate();
+			return true;
+		}catch(Exception e){
+			e.printStackTrace();
+			return false;
+		}
+	}
+	public boolean deleteinfo(String username)
+	{
+		try{
+			pstmt=ct.prepareStatement("delete from user where username = ?");
+			pstmt.setString(1, username);
+			pstmt.executeUpdate();
+			return true;
+		}catch(Exception e){
+			e.printStackTrace();
+			return false;
+		}
+	}
 	public ArrayList findAllUser(){
 		try{
 			pstmt=ct.prepareStatement("select * from user");
